39f6b49aad8c29f82e961d45b01c02bd46ca0f6c
SheepFromHeaven <marcwissler@gmail.com>
2020-09-15 22:44:28 +0200

    Add Ruhmar

---
+++ data/markers/cities.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    lat: -27.059125784374054,
+    lng: -140.27343750000003,
+    tooltip: 'Ruhmar'
+  },
+]
--- data/overlayMaps.js
+++ data/overlayMaps.js
@@ -1,13 +1,18 @@
+import cities from './markers/cities.js';
 import towns from './markers/towns.js';
 import villages from './markers/villages.js';
 
 export default [
   {
+    name: 'Villages',
+    markers: villages,
+  },
+  {
     name: 'Towns',
     markers: towns,
   },
   {
-    name: 'Villages',
-    markers: villages,
-  }
+    name: 'Cities',
+    markers: cities,
+  },
 ]
