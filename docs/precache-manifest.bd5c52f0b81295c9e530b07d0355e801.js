97c8548cab425e3f527f4076a9ea269133face9d
Marc Emmanuel <marc.emmanuel@virtual-identity.com>
2020-09-18 08:32:15 +0200

    fix: 🐛 Set root path of page

---
+++ docs/precache-manifest.bd5c52f0b81295c9e530b07d0355e801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b4504ecc077bc55ae842f6b393c7ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9779a1a3edc9ed7098d9",
+    "url": "./static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "0be4d549b47d75288e1d",
+    "url": "./static/css/main.cddfae1e.chunk.css"
+  },
+  {
+    "revision": "9779a1a3edc9ed7098d9",
+    "url": "./static/js/2.2df5df43.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "./static/js/2.2df5df43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be4d549b47d75288e1d",
+    "url": "./static/js/main.16d55a01.chunk.js"
+  },
+  {
+    "revision": "3523c65fec116250123f",
+    "url": "./static/js/runtime-main.22704009.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34d7864b3a2923b5c77cc71594a1839d.js"
+  "./precache-manifest.bd5c52f0b81295c9e530b07d0355e801.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
